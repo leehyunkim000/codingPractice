2e27602fa7fb7df2c1f6f6a8123a0b872612694c
leehyunkim000 <tsarevich@Lees-MacBook-Pro.local>
2021-06-21 20:54:48 -0400

    Reverse String

--- src/main/reverseString/reverseString.js
+++ src/main/reverseString/reverseString.js
@@ -2,8 +2,16 @@
  * 344. Reverse String
  */
 const reverseString = function(s) {
+    let start = 0
+    let end = s.length - 1
 
-    return "helloffadsfasdfasdfg;"
-};
+    while (start < end) {
+        let temp = s[start]
+        s[start] = s[end]
+        s[end] = temp
+        start++
+        end--
+    }
+}
 
 console.log(reverseString(["h","e","l","l","o"])) // [ 'o', 'l', 'l', 'e', 'h' ]
