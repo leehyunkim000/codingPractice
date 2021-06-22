ccb8c30f0000f515aa533ab5c4ab6d19deb2d2a1
leehyunkim000 <tsarevich@Lees-MacBook-Pro.local>
2021-06-22 19:56:35 -0400

    Valid Polindrome

--- src/main/validPalindrome/validPalindrome.js
+++ src/main/validPalindrome/validPalindrome.js
@@ -2,7 +2,18 @@
  * 125. Valid Palindrome
  */
 const isPalindrome = function(s) {
+    s = s.toLowerCase()
+    s = s.replace(/[^A-Za-z0-9]/g, '')
 
+    let start = 0
+    let end = s.length - 1
+
+    while (start < end) {
+        if (s[start] !== s[end]) return false
+        start ++
+        end --
+    }
+    return true
 };
 
 console.log(isPalindrome("A man, a plan, a canal: Panama")) // true
